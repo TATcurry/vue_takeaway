a9beaa8ae578610bc459e2566b3ceb5b95d963f3
wangbin <wangbin@asiainfo.com>
2018-08-30 10:21:51 +0800

    init demo

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+
+    <router-view/>
+    <FooterGuide v-show="$route.meta.showFooter" />
+  </div>
+</template>
+
+<script>
+
+  import FooterGuide from "./components/FooterGuide/FooterGuide"
+
+  export default {
+    name: 'App',
+
+    components:{
+      FooterGuide
+    }
+  }
+
+
+</script>
+
+<style lang="stylus" ref="stylesheet/stylus">
+  @import "../static/css/reset.css"
+</style>
